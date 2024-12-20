c542925ff09fa73532bb288081939fb60788cbd0
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-12-20 23:14:49 +0545

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Alert" onPress={() => alert('Button Pressed!')} />
+      <Text style={styles.text}>Getting Started with React Native.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#FF5733',
+    fontSize: 20,
   },
 });
